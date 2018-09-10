0302821947bf1ad8f9b2008f66e7992b510a5662
john <john@dev.home.kolon.co.uk>
2018-09-10 10:21:21 +0100

    some updates

--- venv-setup.sh
+++ venv-setup.sh
@@ -1,9 +1,9 @@
-#install python3
-yum install python3 python3-pip -y
+#install python3 & and setup virtual environment
+yum install python34 python34-pip -y
+python3 -m venv .
+. bin/activate
 
-python3 -m venv
-pip3 update -install pip
-. venv/bin/activate
+# upgrade pip and pull project source
+pip3 install --upgrade pip
 pip3 install -r requirements.txt
-mkdir src
-git pull https://github.com/kolonuk/youtube-dl-webui.git:src
+git clone https://github.com/kolonuk/youtube-dl-webui.git src
